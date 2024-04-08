47e3955a6315ecd4f13ed6e585509e6dac0da12d
robert_constantin <robert.constantin@optivamedia.com>
2024-04-08 18:13:40 +0200

    project setup

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kapt)
+}
+
+android {
+    namespace = "com.example.tddmovieapp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.tddmovieapp"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.kotlin.compiler.extension.version.get()
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    testOptions.unitTests {
+        isReturnDefaultValues = true
+        all { tests ->
+            tests.useJUnitPlatform()
+            tests.testLogging {
+                events("passed", "failed", "skipped")
+            }
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.bundles.androidx)
+    implementation(libs.bundles.hilt)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+
+    kapt(libs.hilt.compiler)
+
+    androidTestImplementation(platform(libs.compose.bom))
+    androidTestImplementation(libs.bundles.ui.testing)
+
+    kaptAndroidTest(libs.hilt.android.test.compiler)
+
+    testImplementation(libs.bundles.unit.testing)
+    testRuntimeOnly(libs.junit.jupiter.engine)
+}
