fcfdf1f421c5886b29448bb66abe9c301102b04e
robert_constantin <robert.constantin@optivamedia.com>
2024-04-26 06:29:19 +0200

    extract external collaborator that searches the movies, ini this case a use case

--- app/src/main/java/com/example/tddmovieapp/presentation/feature/search/SearchScreenViewModel.kt
+++ app/src/main/java/com/example/tddmovieapp/presentation/feature/search/SearchScreenViewModel.kt
@@ -22,24 +22,55 @@ class SearchScreenViewModel {
         queryState = newValue
     }
 
-    val movies = listOf(
-        MovieVO(131, "Iron Man", 4.5, "imageUrl"),
-        MovieVO(4532, "Marvel: Avangers", 4.1, "imageUrl1"),
-        MovieVO(5675, "Marvel: Black Panther", 5.0, "imageUrl2")
-    )
-
     fun search(input: String) {
-        val searchedMovies = movies.filter { movie ->
-            movie.title.contains(input, true)
-        }
-
-        if (searchedMovies.isNotEmpty()) {
-            _uiState.update { state ->
-                state.copy(isLoading = false, isEmpty = false, success = searchedMovies)
+        SearchMoviesUseCase().invoke(
+            input,
+            success = { searchedMovies ->
+                _uiState.update { state ->
+                    state.copy(isLoading = false, isEmpty = false, success = searchedMovies)
+                }
+            },
+            empty = {
+                _uiState.update { state ->
+                    state.copy(isLoading = false, isEmpty = true)
+                }
             }
-        } else {
-            _uiState.update { state ->
-                state.copy(isLoading = false, isEmpty = true)
+        )
+//        val searchedMovies = movies.filter { movie ->
+//            movie.title.contains(input, true)
+//        }
+//
+//        if (searchedMovies.isNotEmpty()) {
+//            _uiState.update { state ->
+//                state.copy(isLoading = false, isEmpty = false, success = searchedMovies)
+//            }
+//        } else {
+//            _uiState.update { state ->
+//                state.copy(isLoading = false, isEmpty = true)
+//            }
+//        }
+    }
+
+    class SearchMoviesUseCase() {
+        operator fun invoke(
+            query: String,
+            success: (listMovies: List<MovieVO>) -> Unit,
+            empty: () -> Unit
+        ) {
+            val movies = listOf(
+                MovieVO(131, "Iron Man", 4.5, "imageUrl"),
+                MovieVO(4532, "Marvel: Avangers", 4.1, "imageUrl1"),
+                MovieVO(5675, "Marvel: Black Panther", 5.0, "imageUrl2")
+            )
+
+            val searchedMovies = movies.filter { movie ->
+                movie.title.contains(query, true)
+            }
+
+            if (searchedMovies.isNotEmpty()) {
+                success(searchedMovies)
+            } else {
+                empty()
             }
         }
     }
