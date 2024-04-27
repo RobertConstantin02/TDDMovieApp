38f5bfd173c711f2f4588e28b008ad573056f357
robert_constantin <robert.constantin@optivamedia.com>
2024-04-27 13:33:20 +0200

    test when search and connectivity error, green test

--- app/src/main/java/com/example/tddmovieapp/presentation/mapper/PresentationMapper.kt
+++ app/src/main/java/com/example/tddmovieapp/presentation/mapper/PresentationMapper.kt
@@ -3,4 +3,8 @@ package com.example.tddmovieapp.presentation.mapper
 import com.example.tddmovieapp.domain.model.DomainSearchError
 import com.example.tddmovieapp.presentation.feature.search.SearchError
 
-fun DomainSearchError.toErrorVo(): SearchError = SearchError.ServerError
+fun DomainSearchError.toErrorVo(): SearchError =
+    when(this) {
+        is DomainSearchError.ServerError -> SearchError.ServerError
+        is DomainSearchError.ConnectivityError -> SearchError.ConnectivityError
+    }
--- app/src/test/java/com/example/tddmovieapp/presentation/search/SearchViewModelErrorTest.kt
+++ app/src/test/java/com/example/tddmovieapp/presentation/search/SearchViewModelErrorTest.kt
@@ -2,8 +2,8 @@ package com.example.tddmovieapp.presentation.search
 
 import com.example.tddmovieapp.presentation.feature.search.SearchError
 import com.example.tddmovieapp.presentation.feature.search.SearchScreenEvent
-import com.example.tddmovieapp.presentation.feature.search.SearchScreenState
 import com.example.tddmovieapp.presentation.feature.search.SearchScreenViewModel
+import com.example.tddmovieapp.presentation.search.test_doubles.SearchMoviesUseCaseImplConnectivityErrorStub
 import com.example.tddmovieapp.presentation.search.test_doubles.SearchMoviesUseCaseImplServerErrorStub
 import com.google.common.truth.Truth.assertThat
 import org.junit.jupiter.api.Test
@@ -13,18 +13,27 @@ class SearchViewModelErrorTest {
     @Test
     fun `when search and server error`() {
         //Given
-        val expected = SearchScreenState().copy(
-            isLoading = false,
-            isEmpty = false,
-            error = SearchError.ServerError
-        )
         val input = "Iron Main"
         val viewModel = SearchScreenViewModel(SearchMoviesUseCaseImplServerErrorStub())
         //When
-        viewModel.search(input)
+        viewModel.onEvent(SearchScreenEvent.OnUpdateQuery(input))
         viewModel.onEvent(SearchScreenEvent.OnSearchMovies)
         //Then
         assertThat(viewModel.uiState.value.error).isNotNull()
         assertThat(viewModel.uiState.value.error).isInstanceOf(SearchError.ServerError::class.java)
+    }
+
+    @Test
+    fun `when search and connectivity error`() {
+
+        //Given
+        val input = "Iron Man"
+        //When
+        val viewModel = SearchScreenViewModel(SearchMoviesUseCaseImplConnectivityErrorStub())
+        viewModel.onEvent(SearchScreenEvent.OnUpdateQuery(input))
+        viewModel.onEvent(SearchScreenEvent.OnSearchMovies)
+        //Then
+        assertThat(viewModel.uiState.value.error).isNotNull()
+        assertThat(viewModel.uiState.value.error).isInstanceOf(SearchError.ConnectivityError::class.java)
     }
 }
