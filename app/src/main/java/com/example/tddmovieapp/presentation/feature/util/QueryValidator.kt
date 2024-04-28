7edd632576111d8a0971014945deaa3652395dea
robert_constantin <robert.constantin@optivamedia.com>
2024-04-28 20:38:35 +0200

    add validator test and class

---
+++ app/src/main/java/com/example/tddmovieapp/presentation/feature/util/QueryValidator.kt
@@ -0,0 +1,16 @@
+package com.example.tddmovieapp.presentation.feature.util
+
+import java.util.regex.Pattern
+
+class QueryValidator {
+    fun validate(query: String): Boolean {
+        return if (query.trim().length <= 3 || containsSpecialCharacters(query)) false
+        else true
+    }
+
+    private fun containsSpecialCharacters(query: String): Boolean {
+        val regexSpecialCharacters = "[<({\\^\\-=!|$})?.*+\\[\\]>/]"
+        val pattern = Pattern.compile(regexSpecialCharacters)
+        return pattern.matcher(query).find()
+    }
+}
---
+++ app/src/test/java/com/example/tddmovieapp/presentation/QueryValidatorTest.kt
@@ -0,0 +1,29 @@
+package com.example.tddmovieapp.presentation
+
+import com.example.tddmovieapp.presentation.feature.util.QueryValidator
+import com.google.common.truth.Truth.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+class QueryValidatorTest {
+
+    @ParameterizedTest
+    @CsvSource(
+        "'iron man', true",
+        "'iro', false",
+        "'    ', false",
+        "'   iron ma', true",
+        "'', false",
+        "'iron-man', false",
+        "'* iron', false"
+    )
+    fun `given a proper query, when search, then validator returns true`(query: String, expected: Boolean) {
+        //Given
+        val validator = QueryValidator()
+        //When
+        val validatorResult = validator.validate(query)
+        //Then
+        assertThat(validatorResult).isEqualTo(expected)
+    }
+}
